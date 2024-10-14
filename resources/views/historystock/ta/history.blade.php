b95185e36f92096c0bc8f16f7fafe8b36b3d9f2f
Imam Haris Syafaat <38088979+ImamHaris@users.noreply.github.com>
2024-10-14 15:48:56 +0700

    Add History Log With Detail And Group By Type Product

---
+++ resources/views/historystock/ta/history.blade.php
@@ -0,0 +1,145 @@
+@extends('layouts.master')
+
+@section('konten')
+
+<div class="page-content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                    <div class="page-title-left">
+                        <a href="{{ route('historystock') }}" class="btn btn-light waves-effect btn-label waves-light">
+                            <i class="mdi mdi-arrow-left label-icon"></i> Back
+                        </a>
+                    </div>
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">Master Data</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('historystock') }}">History Stock</a></li>
+                            <li class="breadcrumb-item active">{{ $detail->code ?? '' }}</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        @include('layouts.alert')
+
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-bordered dt-responsive nowrap w-100">
+                    <tbody>
+                        <tr>
+                            <td class="align-middle"><b>Code</b></td>
+                            <td class="align-middle">: {{ $detail->code ?? '' }}</td>
+                        </tr>
+                        <tr>
+                            <td class="align-middle"><b>Description </b></td>
+                            <td class="align-middle">: {{ $detail->description ?? '' }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
+                        <table class="table table-bordered table-striped table-hover dt-responsive nowrap w-100" id="ssTableTA" style="font-size: small">
+                            <thead>
+                                <tr>
+                                    <th class="align-middle text-center">#</th>
+                                    <th class="align-middle text-center">Lot Number</th>
+                                    <th class="align-middle text-center">Type Product</th>
+                                    <th class="align-middle text-center">Qty</th>
+                                    <th class="align-middle text-center">Type Stock</th>
+                                    <th class="align-middle text-center">Date</th>
+                                    <th class="align-middle text-center">Remark</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Server Side -->
+<script>
+    $(function() {
+        var url = '{!! route('historystock.historyTA', encrypt($id)) !!}';
+        var dataTable = $('#ssTableTA').DataTable({
+            language: {
+                processing: '<div id="custom-loader" class="dataTables_processing"></div>'
+            },
+            processing: true,
+            serverSide: true,
+            scrollX: true,
+            pageLength: 5,
+            lengthMenu: [
+                [5, 10, 20, 25, 50, 100, 200, -1],
+                [5, 10, 20, 25, 50, 100, 200, "All"]
+            ],
+            ajax: {
+                url: url,
+                type: 'GET',
+            },
+            columns: [
+                {
+                data: null,
+                    render: function(data, type, row, meta) {
+                        return meta.row + meta.settings._iDisplayStart + 1;
+                    },
+                    orderable: false,
+                    searchable: false,
+                    className: 'align-middle text-center',
+                },
+                {
+                    data: 'lot_number',
+                    name: 'lot_number',
+                    orderable: true,
+                    searchable: true,
+                    className: 'align-top text-bold',
+                },
+                {
+                    data: 'type_product',
+                    name: 'type_product',
+                    orderable: true,
+                    searchable: true,
+                    className: 'align-top',
+                },
+                {
+                    data: 'qty',
+                    name: 'qty',
+                    orderable: true,
+                    searchable: true,
+                    className: 'align-top',
+                },
+                {
+                    data: 'type_stock',
+                    name: 'type_stock',
+                    orderable: true,
+                    searchable: true,
+                    className: 'align-top',
+                },
+                {
+                    data: 'date',
+                    name: 'date',
+                    orderable: true,
+                    searchable: true,
+                    className: 'align-top',
+                },
+                {
+                    data: 'remarks',
+                    name: 'remarks',
+                    orderable: true,
+                    searchable: true,
+                    className: 'align-top',
+                },
+            ]
+        });
+        $('.dataTables_processing').css('z-index', '9999');
+    });
+</script>
+
+@endsection
