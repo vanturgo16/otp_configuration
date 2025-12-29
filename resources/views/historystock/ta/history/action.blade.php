0247cdddbf197ff9b25f3e89730f148795050dfb
Imam Haris Syafaat <38088979+ImamHaris@users.noreply.github.com>
2025-12-29 17:11:17 +0700

    try fix route history stock

--- resources/views/historystock/ta/history/action.blade.php
+++ resources/views/historystock/ta/history/action.blade.php
@@ -1,4 +1,4 @@
-@if($data->id && $data->tableJoin)
+@if(!empty($data->id) && !empty($data->tableJoin))
 <a href="{{ route('historystock.ta.detail', [encrypt($data->id), $data->tableJoin]) }}" class="btn btn-sm btn-info waves-effect btn-label waves-light" title="Detail Stock">
     <i class="mdi mdi-information label-icon"></i> Detail
 </a>
